7810c5ba80b983bb1ee30045fcdff1ff8899942d
LaktionovVladyslav <lakworkmail@gmail.com>
2019-09-07 18:20:53 +0300

    ""

--- chat_parser.py
+++ chat_parser.py
@@ -5,12 +5,13 @@ from telethon.errors import ChatAdminRequiredError
 class ChatParserMethods(TelegramClient):
     def parsing_chat(self, chat_obj):
         chat = self.get_entity(chat_obj.user_names[-1].username)
-        try:
-            participants = self.get_participants(chat)
-        except ChatAdminRequiredError:
-            return []
-        users = [user for user in participants if user.username]
-        return users
+        if chat.megagroup:
+            try:
+                participants = self.get_participants(chat)
+            except ChatAdminRequiredError:
+                return []
+            users = [user for user in participants if user.username]
+            return users
 
 
 
